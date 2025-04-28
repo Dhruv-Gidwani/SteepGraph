61808cb5b79f0e82e0ddf02b28c0e22d70f228f1
Vaibhav <vaibhav27001@gmail.com>
2025-04-28 17:59:00 +0530

    label change

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -4,19 +4,7 @@ import { CanActivate, Router } from '@angular/router';
 @Injectable({
   providedIn: 'root'
 })
-// export class AuthGuard {
-//   constructor(private router: Router) {}
 
-//   canActivateFn(): boolean {
-//     const token = sessionStorage.getItem('access_token');
-//     if (token) {
-//       return true;
-//     } else {
-//       this.router.navigate(['/login']);
-//       return false;
-//     }
-//   }
-// }
 export class AuthGuard implements CanActivate {
     constructor(private router: Router) {}
   
--- src/app/layout/component/app.menu.ts
+++ src/app/layout/component/app.menu.ts
@@ -33,7 +33,7 @@ export class AppMenu {
             {
                 label: 'Authenticaition',
                 items: [
-                    { label: 'Login', icon: 'pi pi-fw pi-sign-in', routerLink: ['/'] },
+                    { label: 'LogOut', icon: 'pi pi-fw pi-sign-in', routerLink: ['/'] },
                 ]
             },
             // {
