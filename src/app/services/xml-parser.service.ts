bc6a2ca157f7bc94490e8658a2132137a1ba67f4
dhruv <dhruvgidwani8700@gmail.com>
2025-05-17 15:00:23 +0530

    profile icon removed

--- src/app/services/xml-parser.service.ts
+++ src/app/services/xml-parser.service.ts
@@ -3,12 +3,11 @@ import { Injectable } from '@angular/core';
 import { Parser } from 'xml2js';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class XmlParserService {
-
-  async parseXml(xmlData: string): Promise<any> {
-    const parser = new Parser({ explicitArray: false });
-    return parser.parseStringPromise(xmlData);
-  }
+    async parseXml(xmlData: string): Promise<any> {
+        const parser = new Parser({ explicitArray: false });
+        return parser.parseStringPromise(xmlData);
+    }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl:'http://192.168.0.230/QAEnvironment'// <-- your development URL
+  apiUrl:'http://192.168.0.230/QAEnvironment'
 };
