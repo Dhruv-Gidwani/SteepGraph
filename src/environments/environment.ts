2848c68c1caf4f8d16614d63e7285f46a045fd6d
dhruv <dhruvgidwani8700@gmail.com>
2025-08-13 17:38:33 +0530

    Recent Changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,4 @@ export const environment = {
     apiUrl: 'http://192.168.0.230/QAEnvironment',
     database: 'OLM_QA'
     //database: 'OLM_QA_20250617'
-
-    //Production environment
-    // production: true,
-    // apiUrl: 'https://external.steepgraph.com/olm12',
-    // database: 'OLM12'
 };
